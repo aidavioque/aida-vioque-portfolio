04ec2126afa5b9e0f9d8af46c4851dc3d17652a7
Alvaro leal <pursuance@gmail.com>
2022-05-22 21:35:04 +0200

    work in progress

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,6 +9,8 @@ import { InView } from 'react-intersection-observer';
 import { motion } from 'framer-motion';
 import cx from 'classnames';
 
+const NEXT_PUBLIC_WIP = process.env.NEXT_PUBLIC_WIP === 'true';
+
 const Section = ({ children, className, id }) => {
   const [isInView, setIsInView] = useState(false);
   const variants = {
@@ -44,89 +46,110 @@ export default function Home() {
       </Head>
       <main className="min-h-screen p-8 overflow-auto relative bg-backround">
         <section className="min-h-screen relative flex justify-center">
-          <div className="container">
-            <h1 className="font-sans text-9xl pb-24 text-primary">
-              <div>Aida</div>
-              <div>Aranguren</div>
-            </h1>
-            <h2 className="font-sans text-4xl pb-24 text-primary">
-              Hi! I'm a great Product designer
-            </h2>
+          <div className="container flex flex-col lg:flex-row justify-between">
+            <div className="min-w-1/2 flex-1 ">
+              <h1 className="font-sans text-7xl lg:text-9xl pb-12 lg:pb-24 text-primary">
+                <div>Aida</div>
+                <div>Aranguren</div>
+              </h1>
+              <h2 className="font-sans text-4xl pb-24 leading-relaxed text-primary">
+                <div>Hi! I am a Jr. Product Designer, </div>
+                <div>UX Writer, & Instructional Designer</div>
+              </h2>
+            </div>
+            {NEXT_PUBLIC_WIP && (
+              <div className="min-w-1/2 lg:pl-16">
+                <img
+                  src="/wip.gif"
+                  className="mb-10"
+                  style={{ height: '320px' }}
+                />
+                <h2 className="font-sans text-4xl pb-24 text-primary">
+                  Work in progress
+                </h2>
+              </div>
+            )}
           </div>
         </section>
-        <Header />
-        <Section id="about" className="min-h-screen text-primary">
-          <div>
-            <h3 className="font-sans text-center text-6xl mb-16">About</h3>
-            <div className="flex justify-center">
-              <div
-                className="h-16 w-16 bg-cover rounded-full mb-16"
-                style={{ backgroundImage: `url(/aida.avif)` }}
-              />
+        {!NEXT_PUBLIC_WIP && <Header />}
+        {!NEXT_PUBLIC_WIP && (
+          <Section id="about" className="min-h-screen text-primary">
+            <div>
+              <h3 className="font-sans text-center text-6xl mb-16">About</h3>
+              <div className="flex justify-center">
+                <div
+                  className="h-16 w-16 bg-cover rounded-full mb-16"
+                  style={{ backgroundImage: `url(/aida.avif)` }}
+                />
+              </div>
+              <div className="font-sans text-xl text-center mb-16">
+                La más linda que se haya visto. Oh my! ¡¡Cómo de linda es!!
+              </div>
+              <div className="flex justify-center">
+                <Link className="font-sans text-xl text-center" href="/about">
+                  Read more
+                </Link>
+              </div>
             </div>
-            <div className="font-sans text-xl text-center mb-16">
-              La más linda que se haya visto. Oh my! ¡¡Cómo de linda es!!
-            </div>
-            <div className="flex justify-center">
-              <Link className="font-sans text-xl text-center" href="/about">
-                Read more
-              </Link>
-            </div>
-          </div>
-        </Section>
-        <Section id="projects" className="min-h-screen text-primary">
-          <div>
-            <h3 className="font-sans text-center text-6xl mb-36">Projects</h3>
-            <div className="flex justify-center">
-              <div className="grid grid-cols-3 gap-16 container">
-                <div>
-                  <img src="/aida.avif" className="mb-10" />
-                  <div className="font-sans text-3xl text-center mb-6">
-                    Product designer
+          </Section>
+        )}
+        {!NEXT_PUBLIC_WIP && (
+          <Section id="projects" className="min-h-screen text-primary">
+            <div>
+              <h3 className="font-sans text-center text-6xl mb-36">Projects</h3>
+              <div className="flex justify-center">
+                <div className="grid grid-cols-3 gap-16 container">
+                  <div>
+                    <img src="/aida.avif" className="mb-10" />
+                    <div className="font-sans text-3xl text-center mb-6">
+                      Product designer
+                    </div>
+                    <div className="font-sans text-xl text-center">
+                      Hola, es que eres super linda como product designer y vas
+                      a ser la mejor
+                    </div>
                   </div>
-                  <div className="font-sans text-xl text-center">
-                    Hola, es que eres super linda como product designer y vas a
-                    ser la mejor
+                  <div>
+                    <img src="/aida.avif" className="mb-10" />
+                    <div className="font-sans text-3xl text-center mb-6">
+                      Alligator
+                    </div>
+                    <div className="font-sans text-xl text-center">
+                      Hola, es que eres super linda como product designer y vas
+                      a ser la mejor
+                    </div>
                   </div>
-                </div>
-                <div>
-                  <img src="/aida.avif" className="mb-10" />
-                  <div className="font-sans text-3xl text-center mb-6">
-                    Alligator
-                  </div>
-                  <div className="font-sans text-xl text-center">
-                    Hola, es que eres super linda como product designer y vas a
-                    ser la mejor
-                  </div>
-                </div>
-                <div>
-                  <img src="/aida.avif" className="mb-10" />
-                  <div className="font-sans text-3xl text-center mb-6">
-                    Oh no
-                  </div>
-                  <div className="font-sans text-xl text-center">
-                    Hola, es que eres super linda como product designer y vas a
-                    ser la mejor, si!
+                  <div>
+                    <img src="/aida.avif" className="mb-10" />
+                    <div className="font-sans text-3xl text-center mb-6">
+                      Oh no
+                    </div>
+                    <div className="font-sans text-xl text-center">
+                      Hola, es que eres super linda como product designer y vas
+                      a ser la mejor, si!
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </Section>
-        <Section id="contact" className="min-h-screen text-primary">
-          <div>
-            <a href="mailto:aida.aranvio@gmail.com">
-              Contact
-              <img
-                src="/ufo.svg"
-                alt="contact"
-                height={60}
-                width={60}
-                className={styles.contact}
-              />
-            </a>
-          </div>
-        </Section>
+          </Section>
+        )}
+        {!NEXT_PUBLIC_WIP && (
+          <Section id="contact" className="min-h-screen text-primary">
+            <div>
+              <a href="mailto:aida.aranvio@gmail.com">
+                Contact
+                <img
+                  src="/ufo.svg"
+                  alt="contact"
+                  height={60}
+                  width={60}
+                  className={styles.contact}
+                />
+              </a>
+            </div>
+          </Section>
+        )}
       </main>
       <Footer />
     </div>
