fbb91eac67a8b0313c76fedc149f2ff460f66c83
Alvaro Leal <pursuance@gmail.com>
2023-06-26 18:58:41 +0200

    Fix contact link

--- app/components/menu/menu-component.jsx
+++ app/components/menu/menu-component.jsx
@@ -1,9 +1,13 @@
 'use client';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Menu = () => {
-  const scrollToBottom = () => scrollTo(0, document.body.scrollHeight);
+  const scrollToBottom = () => {
+    scrollTo(0, document.body.scrollHeight);
+  };
+  const page = usePathname();
   return (
     <ul className="container flex justify-center">
       <li className="pr-6 hover:scale-110">
@@ -13,9 +17,19 @@ const Menu = () => {
         <Link href="/about">About</Link>
       </li>
       <li className="pr-6 hover:scale-110">
-        <button type="button" className="text-primary" onClick={scrollToBottom}>
-          Contact
-        </button>
+        {page === '/about' ? (
+          <Link href="/#contact" scroll={false}>
+            Contact
+          </Link>
+        ) : (
+          <button
+            type="button"
+            className="text-primary"
+            onClick={scrollToBottom}
+          >
+            Contact
+          </button>
+        )}
       </li>
     </ul>
   );
