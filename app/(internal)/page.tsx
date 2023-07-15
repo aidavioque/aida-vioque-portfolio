5ea6b90e3e573191a0bce9eb0fb8ae2d8b20c329
Alvaro Leal <pursuance@gmail.com>
2023-07-15 20:38:24 +0200

    Update case study text and style

--- app/(internal)/page.tsx
+++ app/(internal)/page.tsx
@@ -16,6 +16,14 @@ import {
 
 import Link from 'next/link';
 
+const Highlighted = ({ text }: { text: string }) => (
+  <div className="inline-flex h-20 items-center justify-start gap-6 rounded-[10px] bg-orange-300 bg-opacity-20 px-6 py-4">
+    <div className="text-center text-base font-normal leading-normal text-slate-700">
+      {text}
+    </div>
+  </div>
+);
+
 export default function Home() {
   return (
     <>
@@ -62,40 +70,43 @@ export default function Home() {
       >
         <h2 className="pb-16 text-5xl text-primary">Product design</h2>
         <div className="flex flex-col items-center justify-center lg:pl-16">
-          {/* <Card
+          <Card
             title="Information request proposal"
             description="Informative flow in a shipping company to reduce anxiety, increase confidence, and achieve higher user activation."
             image="/images/projects/package.png"
             content={
               <div className="h-screen overflow-auto md:h-full">
+                <DialogHeader>
+                  <DialogTitle className="pb-2 text-center font-sans text-2xl font-normal text-black">
+                    <p>Minimizing obstacles:</p>Information request proposal in
+                    a shipping company
+                  </DialogTitle>
+                  <DialogDescription className="text-center">
+                    My main goal is to design a simple, informative flow
+                    accompanied by a UX Writing proposal to reduce anxiety,
+                    increase confidence, and achieve higher user activation.
+                  </DialogDescription>
+                </DialogHeader>
                 <div className="container mt-24">
                   <div className="mb-12 flex gap-12">
-                    <div className="space-y-2 pt-1 text-sm">
-                      <div className="text-xl">
-                        Minimizing obstacles: Information request proposal in a
-                        shipping company
+                    <div className="flex max-w-[50%] flex-col justify-center space-y-8 pt-1 text-center text-sm">
+                      <div>
+                        <h4 className="py-2 font-bold uppercase">My role</h4>
+                        <ul className="space-y-2">
+                          <li>Product Designer</li>
+                          <li>UX writer</li>
+                        </ul>
                       </div>
-                      <p>
-                        My main goal is to design a simple, informative flow
-                        accompanied by a UX Writing proposal to reduce anxiety,
-                        increase confidence, and achieve higher user activation.
-                      </p>
-                      <h4 className="pt-4 font-bold uppercase">My role</h4>
-                      <ul className="space-y-2">
-                        <li>Product Designer</li>
-                        <li>UX writer</li>
-                      </ul>
-                      <h4 className="pt-4 font-bold uppercase">
-                        Why did I take this approach
-                      </h4>
-                      <ul className="space-y-2">
-                        <li>Based on Hick&apos;s Law</li>
-                        <li>
-                          The time it takes to make a decision increases with
-                          the number and complexity of choices.
-                        </li>
-                        <li>The Law of Conservation of Complexity</li>
-                      </ul>
+                      <div>
+                        <h4 className="py-2 font-bold uppercase">
+                          Setting up the tone
+                        </h4>
+                        <p className="max-w-[450px] space-y-2">
+                          Among the 4 dimensions of tone, I have chosen a casual
+                          and slightly enthusiastic approach, while maintaining
+                          friendliness and generating trust in the user.
+                        </p>
+                      </div>
                     </div>
                     <div>
                       <img
@@ -105,32 +116,49 @@ export default function Home() {
                     </div>
                   </div>
                   <div className="mb-12 flex gap-12">
-                    <div className="space-y-2 pt-1 text-sm">
+                    <div>
                       <img
                         src="/images/projects/full/packing2.jpg"
                         alt="package"
                       />
                     </div>
-                    <div>
-                      <h4 className="pt-4 font-bold uppercase">
-                        Setting up the tone
+                    <div className="flex max-w-[50%] flex-col justify-center space-y-2 pt-1 text-center text-sm">
+                      <h4 className="py-4 font-bold uppercase">
+                        Why did I take this approach
                       </h4>
-                      <p>
-                        My main goal is to design a simple, informative flow
-                        accompanied by a UX Writing proposal to reduce anxiety,
-                        increase confidence, and achieve higher user activation.
-                      </p>
+                      <div className="space-y-8">
+                        <ul className="space-y-2">
+                          <li>Based on Hick&apos;s Law</li>
+                          <li>
+                            <Highlighted text="The time it takes to make a decision increases with the number and complexity of choices." />
+                          </li>
+                          <li>
+                            The fewer options, the faster the choice and the
+                            less anxiety.
+                          </li>
+                        </ul>
+                        <ul className="space-y-2">
+                          <li>The Law of Conservation of Complexity</li>
+                          <li>
+                            <Highlighted text="For any system there is a certain amount of complexity which cannot be reduced." />
+                          </li>
+                          <li>
+                            Reducing the complexity of the steps and the
+                            language (UX writing) that accompanies the flow.
+                          </li>
+                        </ul>
+                      </div>
                     </div>
                   </div>
                   <div className="mb-12 flex gap-12">
-                    <div className="space-y-2 pt-1 text-sm">
-                      <h4 className="pt-4 font-bold uppercase">
-                        Setting up the tone
+                    <div className="flex max-w-[50%] flex-col justify-center space-y-2 pt-1 text-center text-sm">
+                      <h4 className="py-2 font-bold uppercase">
+                        Divide and rule
                       </h4>
-                      <p className="text-sm">
-                        My main goal is to design a simple, informative flow
-                        accompanied by a UX Writing proposal to reduce anxiety,
-                        increase confidence, and achieve higher user activation.
+                      <p className="max-w-[450px] text-sm">
+                        To alleviate the anxiety associated with providing data,
+                        I have divided the form into two blocks, aiming to
+                        reduce cognitive load and simplify the process.
                       </p>
                     </div>
                     <div>
@@ -147,26 +175,27 @@ export default function Home() {
                         alt="package"
                       />
                     </div>
-                    <div className="space-y-2 pt-1 text-sm">
-                      <h4 className="pt-4 font-bold uppercase">
-                        Setting up the tone
+                    <div className="flex max-w-[50%] flex-col justify-center space-y-2 pt-1 text-center text-sm">
+                      <h4 className="py-2 font-bold uppercase">
+                        Gathering Relevant Information
                       </h4>
-                      <p>
-                        My main goal is to design a simple, informative flow
-                        accompanied by a UX Writing proposal to reduce anxiety,
-                        increase confidence, and achieve higher user activation.
+                      <p className="max-w-[450px]">
+                        I focused on collecting only the relevant information to
+                        avoid potential frictions and ensure that the user stays
+                        on track. The solution will be customized according to
+                        the needs of the potential client.
                       </p>
                     </div>
                   </div>
                   <div className="mb-12 flex gap-12">
-                    <div className="space-y-2 pt-1 text-sm">
-                      <h4 className="pt-4 font-bold uppercase">
-                        Setting up the tone
-                      </h4>
-                      <p>
-                        My main goal is to design a simple, informative flow
-                        accompanied by a UX Writing proposal to reduce anxiety,
-                        increase confidence, and achieve higher user activation.
+                    <div className="flex max-w-[50%] flex-col justify-center space-y-2 pt-1 text-center text-sm">
+                      <h4 className="py-2 font-bold uppercase">Success!</h4>
+                      <p className="max-w-[450px]">
+                        Overall, my approach is centered around minimizing
+                        obstacles and frictions in the information request
+                        process in the shipping company, through a proposal
+                        designed to reduce anxiety, keep the user informed, and
+                        gather only necessary information.
                       </p>
                     </div>
                     <div>
@@ -184,7 +213,7 @@ export default function Home() {
                 </div>
               </div>
             }
-          /> */}
+          />
           <Card
             title="Contest registration flow"
             description="Registration flow to participate in a contest to win a Monstera plant."
@@ -192,10 +221,10 @@ export default function Home() {
             content={
               <div className="h-screen overflow-auto md:h-full">
                 <DialogHeader>
-                  <DialogTitle className="font-sans text-4xl font-normal text-black">
+                  <DialogTitle className="pb-2 text-center font-sans text-2xl font-normal text-black">
                     Monstera contest registration flow
                   </DialogTitle>
-                  <DialogDescription>
+                  <DialogDescription className="text-center">
                     Registration flow for an contest where participants have the
                     chance to win a Monstera plant. This design aims to simplify
                     the registration process, providing a easy and efficient
@@ -223,10 +252,10 @@ export default function Home() {
             content={
               <div className="h-screen overflow-auto md:h-full">
                 <DialogHeader>
-                  <DialogTitle className="font-sans text-4xl font-normal text-black">
+                  <DialogTitle className="pb-2 text-center font-sans text-2xl font-normal text-black">
                     Simple payment screens
                   </DialogTitle>
-                  <DialogDescription>
+                  <DialogDescription className="text-center">
                     Simple design exercise to investigate and test with forms.
                   </DialogDescription>
                 </DialogHeader>
@@ -251,10 +280,10 @@ export default function Home() {
             content={
               <div className="h-screen overflow-auto md:h-full">
                 <DialogHeader>
-                  <DialogTitle className="font-sans text-4xl font-normal text-black">
+                  <DialogTitle className="text-center font-sans text-2xl font-normal text-black pb-2">
                     Smart Protection
                   </DialogTitle>
-                  <DialogDescription>
+                  <DialogDescription className='text-center'>
                     Marketing campaign in different media platforms
                   </DialogDescription>
                 </DialogHeader>
@@ -279,10 +308,10 @@ export default function Home() {
             content={
               <div className="h-screen overflow-auto md:h-full">
                 <DialogHeader>
-                  <DialogTitle className="font-sans text-4xl font-normal text-black">
+                  <DialogTitle className="pb-2 text-center font-sans text-2xl font-normal text-black">
                     Monuments of the world
                   </DialogTitle>
-                  <DialogDescription>
+                  <DialogDescription className="text-center">
                     Interactive illustration of monuments of the world
                   </DialogDescription>
                 </DialogHeader>
