c54e7427da2073600708bc8c26a800ba7532523a
Alvaro <pursuance@gmail.com>
2024-08-30 03:19:48 +0200

    Add mobile menu

--- components/header/header-component.tsx
+++ components/header/header-component.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const Header = () => (
   <header className="container mt-4 grid w-full justify-between bg-white">
     <div className="col-span-12 flex w-full justify-between pb-4">
-      <Link href="/" className="shrink-0">
+      <Link href="/" className="z-50 shrink-0">
         <Image src="/logo.svg" alt="Aida Logo" height={54} width={87} />
       </Link>
       <Menu />
--- components/menu/menu-component.tsx
+++ components/menu/menu-component.tsx
@@ -1,51 +1,148 @@
 'use client';
+import { useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import cx from 'classnames';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import AnimatedUnderline from '@/components/animations/animated-underline';
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import { createPortal } from 'react-dom';
 
 const Menu = () => {
   const pathName = usePathname();
+
+  const [openedMenu, setOpenedMenu] = useState<boolean>(false);
+
+  const MobileMenu = () => {
+    const pathName = usePathname();
+    useEffect(() => {
+      document.body.style.overflow = 'hidden';
+      return () => {
+        document.body.style.overflow = 'auto';
+      };
+    }, []);
+
+    const renderMobileMenu = (
+      <AnimatePresence>
+        <motion.div
+          key="mobile-menu"
+          initial={{ height: 0 }}
+          animate={{ height: '100vh' }}
+          exit={{ height: 0 }}
+          className="fixed inset-0 z-40 w-full overflow-hidden bg-white pt-16 lg:hidden"
+        >
+          <ul className="container flex flex-col gap-[33px] pl-6 pt-16 font-serif font-semibold uppercase leading-tight">
+            <li
+              className={cx('w-fit', {
+                'font-bold ': pathName === '/',
+              })}
+            >
+              <Link href="/" onClick={() => setOpenedMenu(false)}>
+                <AnimatedUnderline>Home</AnimatedUnderline>
+              </Link>
+            </li>
+            <li
+              className={cx('w-fit', {
+                'font-bold ': pathName === '/#projects',
+              })}
+            >
+              <Link
+                href="/#projects"
+                scroll={false}
+                onClick={() => setOpenedMenu(false)}
+              >
+                <AnimatedUnderline>Projects</AnimatedUnderline>
+              </Link>
+            </li>
+            <li
+              className={cx('w-fit', {
+                'font-bold ': pathName === '/about',
+              })}
+            >
+              <Link href="/about" onClick={() => setOpenedMenu(false)}>
+                <AnimatedUnderline>About</AnimatedUnderline>
+              </Link>
+            </li>
+            <li
+              className={cx('w-fit', {
+                'font-bold ': pathName === '/#contact',
+              })}
+            >
+              <Link href="/#contact" onClick={() => setOpenedMenu(false)}>
+                <AnimatedUnderline>Contact</AnimatedUnderline>
+              </Link>
+            </li>
+          </ul>
+        </motion.div>
+      </AnimatePresence>
+    );
+
+    return createPortal(renderMobileMenu, document.body);
+  };
   return (
-    <ul className="flex flex-wrap items-center justify-end gap-4 font-serif text-sm font-semibold uppercase leading-tight lg:gap-[68px] lg:text-base">
-      <li
-        className={cx('hidden lg:block', {
-          'font-bold ': pathName === '/',
-        })}
+    <>
+      <Button
+        variant="ghost"
+        className="z-50 lg:hidden"
+        onClick={() => setOpenedMenu(!openedMenu)}
       >
-        <Link href="/">
-          <AnimatedUnderline>Home</AnimatedUnderline>
-        </Link>
-      </li>
-      <li
-        className={cx({
-          'font-bold ': pathName === '/#projects',
-        })}
-      >
-        <Link href="/#projects" scroll={false}>
-          <AnimatedUnderline>Projects</AnimatedUnderline>
-        </Link>
-      </li>
-      <li
-        className={cx({
-          'font-bold ': pathName === '/about',
-        })}
-      >
-        <Link href="/about">
-          <AnimatedUnderline>About</AnimatedUnderline>
-        </Link>
-      </li>
-      <li
-        className={cx({
-          'font-bold ': pathName === '/#contact',
-        })}
-      >
-        <Link href="/#contact" scroll={false}>
-          <Button variant="primary">Contact</Button>
-        </Link>
-      </li>
-    </ul>
+        {openedMenu ? (
+          <Image
+            src="/images/projects/close.svg"
+            alt="Menu"
+            width={24}
+            height={24}
+          />
+        ) : (
+          <Image
+            src="/images/projects/hamburguer.svg"
+            alt="Menu"
+            width={24}
+            height={24}
+          />
+        )}
+      </Button>
+      {openedMenu && <MobileMenu />}
+      <ul className="hidden items-center justify-end gap-4 font-serif font-semibold uppercase leading-tight lg:flex lg:gap-[68px] lg:text-base">
+        <li
+          className={cx('hidden lg:block', {
+            'font-bold ': pathName === '/',
+          })}
+        >
+          <Link href="/">
+            <AnimatedUnderline>Home</AnimatedUnderline>
+          </Link>
+        </li>
+        <li
+          className={cx({
+            'font-bold ': pathName === '/#projects',
+          })}
+        >
+          <Link href="/#projects" scroll={false}>
+            <AnimatedUnderline>Projects</AnimatedUnderline>
+          </Link>
+        </li>
+        <li
+          className={cx({
+            'font-bold ': pathName === '/about',
+          })}
+        >
+          <Link href="/about">
+            <AnimatedUnderline>About</AnimatedUnderline>
+          </Link>
+        </li>
+        <li
+          className={cx({
+            'font-bold ': pathName === '/#contact',
+          })}
+        >
+          <Link href="/#contact" scroll={false}>
+            <Button variant="primary">Contact</Button>
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 };
 
